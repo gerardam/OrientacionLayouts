6f1cac6d7cc64599b72a2b5e5aa257d32fa02543
Gerardo Alvarez Mendoza <gerardam@outlook.com>
2016-11-30 16:53:50 -0600

    Add project files.

---
+++ BotonesPruebas/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace BotonesPruebas
+{
+    [Activity(Label = "BotonesPruebas", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
